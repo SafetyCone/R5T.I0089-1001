accfa189e79ffa121baf4c76c0c34a4e41921c28
davidcoats <david@getsafetycone.com>
2021-11-01 10:24:41 -0700

    Initial commit of source-stub.

---
+++ source/R5T.I0089-1001.Construction/Code/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace R5T.I0089-1001.Construction
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.I0089-1001/Code/Interfaces/Interface1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.I0089-1001
+{
+    public interface Interface1
+    {
+    }
+}
